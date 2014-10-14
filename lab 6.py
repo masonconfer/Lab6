56fb58c1cc543a1c30517921219ae913a4250d20
masonconfer <masonconfer00@gmail.com>
2014-10-14 13:28:52 -0500

    First Commit
    This reverts commit 6ebe96c14a061a6c1ea94d57014d55f5890ee7a0.
    
    Revert ":lollipop: Added .gitattributes & .gitignore files"
    
    This
    reverts commit b51f10d72ac48d08264e5a889a7499ec0fa2c57b.

---
+++ lab 6.py
@@ -0,0 +1,22 @@
+#print "how many numbers would you like to input?"
+#total = []
+#howManyNumbers = int(raw_input())
+
+#for x in range(howManyNumbers):
+    #print "what number would you like to add to the total?"
+    #userInput1 = int(raw_input())
+    #total = total + userInput1
+    
+#print "the sum is" + str(sum(total))
+
+
+print "Pick a number"
+
+userInput1 = int(raw_input())
+
+total = 0
+
+for x in range():
+    total = total * x
+    
+print total
